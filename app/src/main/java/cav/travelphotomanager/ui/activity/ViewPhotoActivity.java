ecd8b1c1f5c280217b823f1e9028853711797b53
CavInc <cavinc20@gmail.com>
2018-01-27 11:14:25 +0700

    Изменено название и иконка. в активити просмотра добавлена кнопка выхода

--- app/src/main/java/cav/travelphotomanager/ui/activity/ViewPhotoActivity.java
+++ app/src/main/java/cav/travelphotomanager/ui/activity/ViewPhotoActivity.java
@@ -1,7 +1,9 @@
 package cav.travelphotomanager.ui.activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import cav.travelphotomanager.R;
@@ -22,5 +24,21 @@ public class ViewPhotoActivity extends AppCompatActivity {
 
         mImageView.setImageBitmap(Func.getPicSize(file,mImageView));
 
+        setupToolBar();
+    }
+
+    public void setupToolBar(){
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return true;
     }
 }
